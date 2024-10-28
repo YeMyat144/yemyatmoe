6b1f1f6c4c6d9b661b07233398c0a9e1283cf896
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-10-28 13:24:10 +0700

    Fix padding in Footer component

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   const theme = useTheme();
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', mb: 8 }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', mb: 5 }}>
         <IconButton href="https://github.com/yemyat144" aria-label="GitHub">
             <FaGithub size={35} style={{ color: theme.palette.text.primary }} />
         </IconButton>
@@ -21,7 +21,7 @@ const Footer = () => {
             <FaResearchgate size={35} style={{ color: theme.palette.text.primary }} />
         </IconButton>
         <IconButton href="https://www.kaggle.com/yemyatmoe" aria-label="Kaggle">
-            <FaKaggle size={35} style={{ color: theme.palette.text.primary }} />
+            <FaKaggle size={30} style={{ color: theme.palette.text.primary }} />
         </IconButton>
     </Box>
   );
